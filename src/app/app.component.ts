56bf5b00337d3698825dfc3f6058f543899b2371
Denis Valdivia <devalfe@gmail.com>
2017-10-01 21:26:50 -0500

    style material

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Http } from '@angular/http';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -6,4 +7,18 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Alphe Store';
+  color: String = 'Red';
+  myData: Array<any>;
+  //constructor(private http: Http) {
+  //   this.http.get('https://jsonplaceholder.typicode.com/photos')
+  //     .map(response => response.json())
+  //     .subscribe(res => this.myData = res)
+  // }
+  generarRandom(): String {
+    return Math.floor(Math.random() * 255).toString(16);
+  }
+
+  colorHex(): String {
+    return "#" + this.generarRandom() + this.generarRandom() + this.generarRandom();
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,19 +2,36 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MdButtonModule, MdCardModule, MdMenuModule, MdToolbarModule, MdIconModule } from '@angular/material';
 import { InicioComponent } from './inicio/inicio.component';
 import { NavbarComponent } from './navbar.component';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    InicioComponent,
-    NavbarComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [
+        AppComponent,
+        InicioComponent,
+        NavbarComponent
+    ],
+    imports: [
+        BrowserModule,
+        BrowserAnimationsModule,
+        MdButtonModule,
+        MdMenuModule,
+        MdCardModule,
+        MdToolbarModule,
+        MdIconModule
+    ],
+    exports: [
+        BrowserModule,
+        BrowserAnimationsModule,
+        MdButtonModule,
+        MdMenuModule,
+        MdCardModule,
+        MdToolbarModule,
+        MdIconModule
+    ],
+    providers: [],
+    bootstrap: [AppComponent]
 })
 export class AppModule { }
